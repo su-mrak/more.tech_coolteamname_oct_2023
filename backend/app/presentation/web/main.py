cdaae6350e3d8c1e21a5c61b0168d148c7df80dc
pibragimov <pibragimov@whoosh.bike>
2023-10-14 14:21:51 +0300

    feat: add route by teller

--- backend/app/presentation/web/main.py
+++ backend/app/presentation/web/main.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from redis import asyncio as aioredis
 
@@ -19,13 +18,13 @@ with open(PARENT / "rapidoc.html", "r") as f:
 def create_app() -> FastAPI:
     app = FastAPI()
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=["*"],
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
     app.include_router(router)
 
     @app.on_event("startup")
