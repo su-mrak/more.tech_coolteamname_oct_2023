a4e03fa70c92e1e86172ff41596696525c664f84
sumrak <nice.purpur@yandex.ru>
2023-10-14 23:29:54 +0300

    fix time perdiods for prediction

--- analyze/find_line_time.py
+++ analyze/find_line_time.py
@@ -11,7 +11,7 @@ from pydantic import Field
 from backend.app.schemas.office import Office, OpenHours
 
 
-# 0: 0mis for waiting, 1: 0-15mins for waiting, 2: 15-30min, 3: 30-45min, 4: 45-60mis, 5: >60min;
+# 0: 0-10 mins for waiting, 1: 10-20 min, 2: 20-30 min, 4: 30-50min, 5: >50min;
 def generate_prediction_time_for_hours(working_hours: list) -> dict:
 
     working_hours_list_as_x = np.arange(working_hours[0], working_hours[1], 1)
@@ -20,18 +20,18 @@ def generate_prediction_time_for_hours(working_hours: list) -> dict:
     for hour in working_hours_list_as_x:
         
         if hour < 11:
-            coefficient_dict[hour] = randint(4, 6)
+            coefficient_dict[hour] = randint(2, 3)
             continue
 
         if hour >= 12 and hour <= 14:
-            coefficient_dict[hour] = randint(4, 6)
+            coefficient_dict[hour] = randint(2, 3)
             continue
 
         if hour > 17 and hour < 20:
-            coefficient_dict[hour] = randint(4, 6)
+            coefficient_dict[hour] = randint(2, 3)
             continue
 
-        coefficient_dict[hour] = randint(0, 4)
+        coefficient_dict[hour] = randint(0, 1)
     
     return coefficient_dict
     
